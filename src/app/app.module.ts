693cab01d96f016669d03e49a18c6e0827585b45
Aurapinilla <aura.pinilla12@gmail.com>
2023-09-11 16:21:33 -0500

    login form actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginViewComponent } from './views/login-view/login-view.component';
+import { LoginFormComponent } from './components/login/login-form/login-form.component';
 
 const routes: Routes = [
   {
@@ -16,11 +18,13 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     LoginViewComponent,
+    LoginFormComponent,
    // LoginViewComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login-form/login-form.component.ts
+++ src/app/components/login/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  send(): any {
+    console.log(this.loginForm.value);
+    
+  }
 }
