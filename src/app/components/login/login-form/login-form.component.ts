3c275b2f801b2740723c67db5c9b5dabb662c12c
Aurapinilla <aura.pinilla12@gmail.com>
2023-09-11 18:08:02 -0500

    loginform actualizado

--- src/app/components/login/login-form/login-form.component.ts
+++ src/app/components/login/login-form/login-form.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
-  loginForm: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
-  });
+export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
-  send(): any {
-    console.log(this.loginForm.value);
-    
+  ngOnInit() {}
+
+  save(event: Event) {
+    event.preventDefault();
+    if (this.loginForm.valid) {
+      const value = this.loginForm.value;
+      console.log(value);
+    }
+  }
+
+  get emailInput() {
+    return this.loginForm.get('email');
+  }
+
+  get passwordInput() {
+    return this.loginForm.get('password');
   }
 }
+
