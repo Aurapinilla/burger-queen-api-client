0c449b405abc687c0d72d05cebd94c693f84c567
Aurapinilla <aura.pinilla12@gmail.com>
2023-10-13 10:13:10 -0500

    ready orders btn test works

--- src/app/components/ready-orders-btn/ready-orders-btn.component.spec.ts
+++ src/app/components/ready-orders-btn/ready-orders-btn.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { ReadyOrdersBtnComponent } from './ready-orders-btn.component';
 
 describe('ReadyOrdersBtnComponent', () => {
   let component: ReadyOrdersBtnComponent;
   let fixture: ComponentFixture<ReadyOrdersBtnComponent>;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,10 +13,19 @@ describe('ReadyOrdersBtnComponent', () => {
     });
     fixture = TestBed.createComponent(ReadyOrdersBtnComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should nagivate to "ready to deliver orders view"', () => {
+
+    const nagivationSpy = jest.spyOn(router, 'navigate');
+
+    component.readyOrders();
+    expect(nagivationSpy).toHaveBeenCalledWith(['ready-to-deliver']);
+  })
 });
