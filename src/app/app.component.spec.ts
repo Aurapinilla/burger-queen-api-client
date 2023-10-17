e5b4e30c3ca1fe710cd3f376d4a29abc94016062
Aurapinilla <aura.pinilla12@gmail.com>
2023-10-17 10:56:50 -0500

    app.component tests updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,11 +17,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('burger-queen-api-client');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('burger-queen-api-client app is running!');
-  });
 });
